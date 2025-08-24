5ccbcb899c37c959d904ec655aff700b2c17924b
Guna1226 <kabildesh87@gmail.com>
2025-08-24 19:18:03 +0530

    updatedd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,8 @@ export default function SearchBar() {
     <header className=" flex items-center justify-between px-4 py-3 backdrop-blur-md bg-white/10 text-white sticky top-0 z-50">
        {/* Display current city */}
       <div className="flex items-center gap-2">
-  <img src="/public/weather1.png" alt="Weather Icon" className="w-6 h-6" />
+  <img src="/weather1.png" alt="Weather Icon" className="w-6 h-6" />
+
   <span className="font-medium text-transparent bg-clip-text bg-white">
   Weather App
 </span>
